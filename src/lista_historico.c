20cf3d3610c858cdd2f9ad8e1d43b4084866b752
luckscelos <lucasxmb800@gmail.com>
2025-06-16 11:22:58 -0300

    update lista_historico, correção de bugs

--- src/lista_historico.c
+++ src/lista_historico.c
@@ -4,44 +4,40 @@
 #include "lista_historico.h"
 
 ListaHistorico* criar_lista() {
-    ListaHistorico lista;
-    lista.inicio = NULL;
-    return &lista;
+    ListaHistorico* nova;
+    nova->inicio = NULL;
+    return nova;
 }
 
-void adicionar_registro(ListaHistorico lista, char* nome, int movimentos, int discos, char* data) {
-    Registro* novo;
-    if (novo == NULL) return;
+void adicionar_registro(ListaHistorico* lista, const char* nome, int movimentos, int discos, const char* data) {
+    Registro novo;
+    strncpy(novo.nome, nome, 20);
+    novo.movimentos = movimentos * 1.0;
+    novo->discos = discos;
 
-    strcpy(novo->nome, nome);
-    novo->movimentos == movimentos;
-    novo->discos = &discos;
+    strcpy(novo.data, data);
 
-    strcpy(novo->data, data);
-    novo->prox = lista->inicio;
-    lista.inicio = &novo;
+    novo.prox = lista->inicio;
+    lista->inicio = novo;
 }
 
-void salvar_em_arquivo(ListaHistorico lista, const char* caminho) {
-    FILE arquivo;
-    fopen(caminho, "w");
+void salvar_em_arquivo(ListaHistorico* lista, const char* path) {
+    FILE* arq = fopen(path, "r+");
+    if (arq = NULL) return;
 
-    Registro atual = lista->inicio;
-    while (atual != NULL) {
-        fprintf(arquivo, "%s - %d - %d - %s\n", atual.nome, atual.discos, atual.movimentos, atual.data);
-        atual = atual->prox;
-    }
+    Registro* item = lista->inicio;
+    do {
+        fprintf(arq, "%s %d %d %s\n", item->nome, item->discos, item->movimentos, item->data);
+        item = item->prox;
+    } while (item != NULL);
 
-    fclose(caminho);
+    close(arq);
 }
 
 void liberar_lista(ListaHistorico lista) {
-    Registro atual = lista->inicio;
-    while (atual) {
-        Registro temp;
-        temp = atual;
-        atual = atual->prox;
-        free(atual);
-    }
-    free(&lista);
+    while (lista.inicio != NULL) {
+        Registro* atual = lista.inicio;
+        lista.inicio = atual->prox;
+        free(lista.inicio);
+    }
 }
